2128be49669b0e080b834f4068c6100455aa994c
matil1011 <mutiulamidi071256@gmail.com>
2022-09-14 15:56:00 -0700

    Task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+ * positive_or_negative - prints the number as negative or positive.
+
+*/
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
